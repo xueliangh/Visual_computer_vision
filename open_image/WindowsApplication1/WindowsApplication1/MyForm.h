bce75eaa425569225cad68a428272a72ccc8f3cd
蕭學良 <s14115122@stu.edu.tw>
2018-07-08 19:22:10 +0800

    20180708

---
+++ open_image/WindowsApplication1/WindowsApplication1/MyForm.h
@@ -0,0 +1,120 @@
+#pragma once
+
+namespace WindowsApplication1
+{
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	/// <summary>
+	/// MyForm 的摘要
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO:  在此加入建構函式程式碼
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	protected:
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(235, 510);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(107, 44);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"開啟圖片";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click_1);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(37, 24);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(533, 464);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(625, 576);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+		//宣告點陣圖的格式 
+		Bitmap^ image1;
+		//new一個新的openFileDialog1開啟檔案
+		OpenFileDialog^ openFileDialog1 = gcnew OpenFileDialog;
+		//設定Filter，用以限定使用者開啟的檔案
+		openFileDialog1->Filter = "點陣圖 (*.bmp)| *.bmp| All files (*.*)| *.*";
+		//預設檔案名稱為空值
+		openFileDialog1->FileName = "";
+		//設定跳出選擇視窗的標題名稱
+		openFileDialog1->Title = "載入影像";
+		//設定Filter選擇模式(依照Filter數，如本例選擇1表示起始出現的為點陣圖，選擇2表示All filts)
+	    openFileDialog1->FilterIndex = 1;
+
+
+		//跳出檔案選擇視窗(ShowDialog)，並且如果使用者點選檔案，並且檔案名稱超過0個字元，則判斷是為True，進入處理程序
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK && openFileDialog1->FileName->Length > 0)
+		{
+			//將選取的檔案讀取並且存至Image1
+			image1 = safe_cast<Bitmap^>(Image::FromFile(openFileDialog1->FileName));
+			//將影像顯示在pictureBox1
+			pictureBox1->Image = image1;
+		}
+	}
+	};
+}
